6f999752278bdd15f860c83e25b657ce144ebd46
TheoCaesar <theocaesar7@gmail.com>
2024-09-06 13:01:52 +0100

    footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgFor],
+  imports: [RouterOutlet, NgFor, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
